9a8eb0af897fa57d4b913a98fa2704b202d7670b
mtsue <bolerock309@gmail.com>
2017-12-18 02:02:35 +0900

    Add api index file

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    getBalance: require('./balance'),
+    getOpens: require('./opens'),
+    getOrderBooks: require('./order_books'),
+    getTransactions: require('./transactions'),
+    sendCancel: require('./cancel'),
+    sendOrders: require('./orders')
+};
